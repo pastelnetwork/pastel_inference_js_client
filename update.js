d20c42b7e829db11d24fe44bb1b849914479c0cc
Dicklesworthstone <jeff141421@gmail.com>
2024-06-05 15:37:33 +0000

    x

--- update.js
+++ update.js
@@ -3,7 +3,7 @@ const path = require("path");
 const currentDir = __dirname;
 
 // Configuration
-const useDebugMode = process.env.USE_DEBUG_MODE === "1";
+const useDebugMode = process.env.USE_DEBUG_MODE === 1;
 const repoUrl =
   "https://github.com/pastelnetwork/pastel_inference_js_client.git";
 const branch = "master"; // Changed from "main" to "master"
