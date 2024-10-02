dd74ef64ce05e414a6ba0d537e607daa79583b57
Dicklesworthstone <jeff141421@gmail.com>
2024-10-02 11:01:35 -0500

    Changed rescan start height

--- utility_functions.js
+++ utility_functions.js
@@ -1383,7 +1383,7 @@ async function importPromotionalPack(jsonFilePath) {
         `Importing private key for tracking address: ${pack.psl_credit_usage_tracking_address}`
       );
 
-      const startingBlockHeight = 732000;
+      const startingBlockHeight = 700000;
       const importResult = await importPrivKey(
         pack.psl_credit_usage_tracking_address_private_key,
         "Imported from promotional pack",
